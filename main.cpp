f1a47351b358427556a22e4006c2dfaa7e38d873
grotiuss <grotius.hasiholan@gmail.com>
2022-07-06 16:07:27 +0700

    Adding functions to find feasible cells (stepping stone purpose)

--- main.cpp
+++ main.cpp
@@ -16,6 +16,8 @@ typedef storage destination;
 
 struct cell {
     long int price = 0;
+    int x;
+    int y;
     int amount = 0;
     bool isEmpty = true;
 };
@@ -52,7 +54,7 @@ int count_storage_capacity(table table_, int supp_dem = 0) {
                 result += table_.supply[i].capacity;
             }
             break;
-        case 1 : 
+        case 1 :
             for (int i = 0; i <= n; i++) {
                 result += table_.demand[i].capacity;
             }
@@ -104,7 +106,7 @@ void footer() {
     cout << endl << endl << "Created by. Grotius" << endl;
 }
 
-void show_table(table table_, string table_name = "Initial Model") {
+void show_table(table table_, string table_name = "") {
     int width = 15;
     int n_source = 0;
     int n_destination = 0;
@@ -185,6 +187,8 @@ table initial_table() {
     for(int i=0; i < n_source; i++) {
         for(int j=0; j < n_destination; j++) {
             cout << "Cell " <<"["<<i <<"]["<< j <<"]"<< "[price] : " ; cin >> result.shipment[i][j].price;
+            result.shipment[i][j].x = i;
+            result.shipment[i][j].y = j;
             result.shipment[i][j].isEmpty = false;
         }
     }
@@ -193,14 +197,14 @@ table initial_table() {
 
 // Initial state
 int min_max (int a, int b, int param = 0) {
-    if (param == 0) {
+    if (param == 0) { // min
         if (a < b) {
             return a;
         } else {
             return b;
         }
     } else {
-        if (a > b) {
+        if (a > b) { // max
             return a;
         } else {
             return b;
@@ -242,6 +246,105 @@ table lcm (table table_) {
     return result;
 }
 
+//Iterations
+    //Finding feasible cell arround the zero cell
+    cell stepping_stone_find(cell shipment_, table table_, int direction = 0) {
+        int result_i = shipment_.x;
+        int result_j = shipment_.y;
+        cell empty;
+
+        switch (direction) {
+            case 0: // atas
+                if (result_i == 0)
+                    return empty;
+                result_i --;
+                while (table_.shipment[result_i][result_j].amount == 0 && result_i > 0) {
+                    result_i --;
+                }
+                if (table_.shipment[result_i][result_j].amount != 0) {
+                    return table_.shipment[result_i][result_j];
+                } else {
+                    return empty;
+                }
+                break;
+            case 1: // bawah
+                if (result_i == count_storage(table_, 0))
+                    return empty;
+                result_i ++;
+                while (table_.shipment[result_i][result_j].amount == 0 && result_i < count_storage(table_, 0)) {
+                    result_i ++;
+                }
+                if (table_.shipment[result_i][result_j].amount != 0) {
+                    return table_.shipment[result_i][result_j];
+                } else {
+                    return empty;
+                }
+                break;
+            case 2: // kiri
+                if(result_j == 0) 
+                    return empty;
+                result_j --;
+                while (table_.shipment[result_i][result_j].amount == 0 && result_j > 0) {
+                    result_j --;
+                }
+                if (table_.shipment[result_i][result_j].amount != 0) {
+                    return table_.shipment[result_i][result_j];
+                } else {
+                    return empty;
+                }
+                break;
+            case 3: // kanan
+                if(result_j == count_storage(table_, 1))
+                    return empty;
+                result_j ++;
+                while (table_.shipment[result_i][result_j].amount == 0 && result_j < count_storage(table_, 1)) {
+                    result_j ++;
+                }
+                if (table_.shipment[result_i][result_j].amount != 0) {
+                    return table_.shipment[result_i][result_j];
+                } else {
+                    return empty;
+                }
+                break;
+            default:
+                return empty;
+        }
+    }
+
+table stepping_stone(table table_) {
+    cout << endl;
+
+    table result = table_;
+    int n_source = count_storage(result, 0);
+    int n_destination = count_storage(result, 1);
+
+    //Getting all cell-indeces with zero (0) amount
+    int i_[min_max(n_source, n_destination, 0)], j_[min_max(n_source, n_destination, 1)];
+    int count = 0;
+    for (int i = 0; i < n_source; i++) {
+        for (int j = 0; j < n_destination; j++) {
+            if (result.shipment[i][j].amount == 0) {
+                i_[count] = i;
+                j_[count] = j;
+                count++;
+            }
+        }
+    }
+
+    cell result_ = stepping_stone_find(table_.shipment[2][0], table_, 0);
+    cout << result_.x << " " << result_.y << " " << result_.amount << endl;
+    result_ = stepping_stone_find(table_.shipment[2][0], table_, 1);
+    cout << result_.x << " " << result_.y << " " << result_.amount << endl;
+    result_ = stepping_stone_find(table_.shipment[2][0], table_, 2);
+    cout << result_.x << " " << result_.y << " " << result_.amount << endl;
+    result_ = stepping_stone_find(table_.shipment[2][0], table_, 3);
+    cout << result_.x << " " << result_.y << " " << result_.amount << endl;
+    return result;
+    // show_table(result, "Stepping Stone");
+    // show_table(result);
+    // show_table(result);
+}
+
 int main()
 {
     table model;
@@ -250,10 +353,12 @@ int main()
     // step 1
     system("CLS");
     title();
-    show_table(model);
+    show_table(model, "Initial Table");
     model = nwc(model);
+    table model_ = stepping_stone(model);
     // cout << endl << count_storage(model, 1);
     cout << endl;
+
     cout << "n_source = " << count_storage(model, 0) << endl;
     cout << "n_destinantion = " << count_storage(model, 1) << endl;
     cout << "total_source_capacity = " << count_storage_capacity(model, 0) << endl;
